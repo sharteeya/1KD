3dc27ba6f53f01e65c7fb6d2d4f762c208f784d9
sharteeya <sharteeya@users.noreply.github.com>
2020-01-31 12:24:05 +0800

    add escape

--- js/bmFetcher.js
+++ js/bmFetcher.js
@@ -7,6 +7,10 @@ function getAnalysisData(tid, uid){
     uxhr.open("GET",link);
     uxhr.onload = async function(){
         data = await JSON.parse(uxhr.responseText);
+        if(data.unit.unit_type!="video"){
+            alert("這不是觀看影片的單元");
+            return;
+        }
         let students = {};
         for(let i = 0 ; i < data.members.length ; i++){
             students[data.members[i].uqid] = data.members[i].full_name;
@@ -34,27 +38,29 @@ function getAnalysisData(tid, uid){
 
 
 let styles = document.createElement("style");
-styles.innerHTML = `#1KD_DIV{
+styles.innerHTML = `#KD_DIV{
                         border: 1px solid black;
                         padding: 10px 0px 10px 0px;
                         background-color: lightgrey;
+                        font-family: 微軟正黑體;
                     }`;
 document.getElementsByClassName("collection-title")[0].appendChild(styles);
 let xhr = new XMLHttpRequest();
 let groupID = window.location.hash.split('/')[2];
 let data;
-let html = "";
+let html = "<h2>下載學生觀看資料</h2>";
 xhr.open("GET",`http://1know.net/private/group/${groupID}/task`);
 xhr.onload = async function(){
     data = await JSON.parse(xhr.responseText);
     let div = document.createElement('div');
-    div.id = "1KD_DIV";
+    div.id = "KD_DIV";
     for(let i = 0 ; i < data.length ; i++){
-        html += `<ul>${data[i].name}`;//(${data[i].uqid})
+        html += `<ul><h4>${data[i].name}</h4>`;//(${data[i].uqid})
         for(let j = 0 ; j < data[i].units.length ; j++){
             html += `<li>${data[i].units[j].name} <button type="button" onclick="getAnalysisData('${data[i].uqid}','${data[i].units[j].uqid}')">下載統計資料</button></li>`
         }//(${data[i].units[j].uqid})
         html += `</ul>`;
+        html += `<hr>`;
     }
     div.innerHTML = html;
     document.getElementsByClassName("collection-title")[0].appendChild(div);
