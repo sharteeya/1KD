32766c938e0f6fcb3bc4cc869517daa29cecf634
sharteeya <sharteeya@users.noreply.github.com>
2020-01-31 11:53:44 +0800

    Update bmFetcher.js

--- js/bmFetcher.js
+++ js/bmFetcher.js
@@ -1,15 +1,45 @@
+
+function getAnalysisData(tid, uid){
+    let link = `http://1know.net/private/group/task/${tid}/analytics/unit/${uid}`;
+    let uxhr = new XMLHttpRequest();
+    let data;
+    let arr = [];
+    uxhr.open("GET",link);
+    uxhr.onload = function(){
+        data = JSON.parse(uxhr.responseText);
+        let students = {};
+        for(let i = 0 ; i < data.members.length ; i++){
+            students[data.members[i].uqid] = data.members[i].full_name;
+        }
+        arr.push(["姓名", "開始時間", "結束時間", "影片開始時間", "影片結束時間"]);
+        for(let i = 0 ; i < data.shs.length ; i++){
+            let d = data.shs[i];
+            arr.push([students[d.uqid], d.real_time_s, d.real_time_e, d.video_time_s, d.video_time_e]);
+        }
+        console.log(arr);
+    }
+    uxhr.send();
+}
+
 let xhr = new XMLHttpRequest();
 let groupID = window.location.hash.split('/')[2];
 let data;
+let html = `<style>
+                #1KD_DIV{
+                    border: 1px solid black;
+                    padding: 10px 0px 10px 0px;
+                    background-color: lightgrey;
+                }
+            <style>`;
 xhr.open("GET",`http://1know.net/private/group/${groupID}/task`);
 xhr.onload = async function(){
     data = await JSON.parse(xhr.responseText);
     let div = document.createElement('div');
-    let html = "";
+    div.id = "1KD_DIV";
     for(let i = 0 ; i < data.length ; i++){
         html += `<ul>${data[i].name}(${data[i].uqid})`;
         for(let j = 0 ; j < data[i].units.length ; j++){
-            html += `<li>${data[i].units[j].name}(${data[i].units[j].uqid})</li>`
+            html += `<li>${data[i].units[j].name}(${data[i].units[j].uqid})<button type="button" onclick="getAnalysisData(${data[i].uqid},${data[i].units[j].uqid})">DL</button></li>`
         }
         html += `</ul>`;
     }
