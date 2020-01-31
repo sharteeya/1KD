6f54d935620e18d98f97a811f00aa964ec48165b
sharteeya <sharteeya@users.noreply.github.com>
2020-01-31 11:22:44 +0800

    Update bmFetcher.js

--- js/bmFetcher.js
+++ js/bmFetcher.js
@@ -4,15 +4,12 @@ let data;
 xhr.open("GET",`http://1know.net/private/group/${groupID}/task`);
 xhr.onload = function(){
     data = JSON.parse(xhr.responseText);
-    console.log("XHR");
+    let div = document.createElement('div');
+    div.innerText = data+"123";
+    document.getElementsByClassName("collection-title")[0].appendChild(div);
 };
 xhr.send();
-console.log("TEST");
-let div = document.createElement('div');
-div.innerText = data+"123";
-document.getElementsByClassName("collection-title")[0].appendChild(div);
+
 //titleArea = document.getElementsByClassName("collection-title")[0];
-
-
 
 //javascript:(function(){let js=document.createElement("script");window.bookmarkletOptions={};js.src="https://sharteeya.github.io/1KD/js/bmFetcher.js";document.body.appendChild(js)})();
