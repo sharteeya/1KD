ce33c592c9224360767cc81adf927d70187e0e12
sharteeya <sharteeya@users.noreply.github.com>
2020-03-30 21:24:45 +0800

    patch

--- js/bmFetcher.js
+++ js/bmFetcher.js
@@ -1,45 +1,112 @@
 var MEMBER_DATA, STUDENT_ID_LIST = {}, TASK_LIST, GROUP_ID;
-
-async function getAnalysisData(tid, uid, gid){
-    let link, mlink;
-    if(window.location.host == "1know.net") link = `http://1know.net/private/group/task/${tid}/analytics/unit/${uid}`;
-    else link = `http://www.1know.net/private/group/task/${tid}/analytics/unit/${uid}`;
-    let uxhr = new XMLHttpRequest(), mxhr = new XMLHttpRequest();
-    let data, memberData = {}, arr = [];
-    //GET MEMBER
-    if(window.location.host == "1know.net") mlink = `http://1know.net/private/group/${gid}/member`;
-    else mlink = `http://www.1know.net/private/group/${gid}/member`
-    mxhr.open("GET", mlink);
-    mxhr.onload = function(){
-        let d = JSON.parse(mxhr.responseText);
-        for(let i = 0 ; i < d.length ; i++) {
-            memberData[d[i].full_name] = d[i].email.split('@')[0] ;
-        }
-        return memberData;
+const PLUGIN_STYLE = `
+    #KD_DIV{
+        border: 1px solid grey;
+        background-color: white;
+        font-family: 微軟正黑體;
+        box-shadow: 1px 2px 2px 2px grey;
     }
-    mxhr.send();
+
+    .KD_HR{
+        border-top: 1px solid black;
+    }
+
+    .KD_BTN{
+        font-family: 微軟正黑體;
+        background-color: #037bfc;
+        border: none;
+        margin-left: 10px;
+        font-weight: bold;
+        margin-right: 10px;
+        border-radius: 5px;
+        color: white;
+        padding: 3px 8px 3px 8px;
+    }
+
+    .KD_BTN_2{
+        font-family: 微軟正黑體;
+        background-color: orange;
+        border: none;
+        margin-left: 10px;
+        font-weight: bold;
+        margin-right: 10px;
+        border-radius: 5px;
+        color: white;
+        padding: 3px 8px 3px 8px;
+    }
+
+    .KD_BTN:hover{
+        background-color: #0057b5;
+    }
+
+    .KD_LI{              
+        font-weight: bolder;
+        margin-bottom: 10px;
+        padding-bottom: 2px;
+        display: block;
+        padding-left: 15px;
+        border-bottom: 1px gray dotted;
+    }
+
+    .KD_H4{
+        font-weight: bold;
+        padding-left: 5px;
+        margin-top: 20px;
+    }
+
+    #KD_LOAD{
+        padding: 15px 0px 15px 0px;
+        font-famliy: 微軟正黑體;
+    }`;
+
+function getAnalysisData(tid, uid){
+    let xhr = new XMLHttpRequest();
+    if(window.location.host == "1know.net") xhr.open("GET", `http://1know.net/private/group/task/${tid}/analytics/unit/${uid}`);
+    else xhr.open("GET", `http://www.1know.net/private/group/task/${tid}/analytics/unit/${uid}`);
+
     //GET DETAIL DATA
-    uxhr.open("GET",link);
-    uxhr.onload = async function(){
-        data = await JSON.parse(uxhr.responseText);
-        if(data.unit.unit_type!="video"){
+
+    xhr.onload = function(){
+        let data = JSON.parse(xhr.responseText);
+        if(data.unit.unit_type != "video"){
             alert("這不是觀看影片的單元");
             return;
         }
-        let students = {};
+
+        let students = {}, arr = [];
 
         data.members.map((student, i) => {
             students[student.uqid] = student.full_name;
         });
 
-        arr.push(["\ufeff姓名", "學號", "開始時間", "結束時間", "花費時間", "影片開始時間", "影片結束時間", "影片觀看時間", "影片名稱", "影片長度"]);
+        arr.push([
+            "\ufeff姓名", 
+            "學號", 
+            "開始時間", 
+            "結束時間", 
+            "花費時間", 
+            "影片開始時間", 
+            "影片結束時間", 
+            "影片觀看時間", 
+            "影片名稱", 
+            "影片長度"
+        ]);
 
-        for(let i = 0 ; i < data.shs.length ; i++){
-            let d = data.shs[i];
-            let r_time_s = new Date(d.real_time_s);
-            let r_time_e = new Date(d.real_time_e);
-            arr.push([students[d.uqid].replace(/\s/g, ''), memberData[students[d.uqid]], r_time_s.toString(), r_time_e.toString(), Math.round(d.real_time_d * 10) / 10, Math.round(d.video_time_s * 10) / 10, Math.round(d.video_time_e * 10) / 10, Math.round(d.video_time_d * 10) / 10, data.unit.name, data.unit.content_time]);
-        }
+        data.shs.map((info, i) => {
+            arr.push([
+                students[info.uqid].replace(/\s/g, ''), 
+                STUDENT_ID_LIST[students[d.uqid]], 
+                new Date(info.real_time_s).toString(), 
+                new Date(info.real_time_e).toString(), 
+                Math.round(info.real_time_d * 10) / 10, 
+                Math.round(info.video_time_s * 10) / 10, 
+                Math.round(info.video_time_e * 10) / 10, 
+                Math.round(info.video_time_d * 10) / 10,
+                data.unit.name, 
+                data.unit.content_time,
+            ]);
+        });
+
         let csvContent = "data:text/csv;charset=utf-8,";
 
         arr.forEach(function(rowArray) {
@@ -53,15 +120,14 @@ async function getAnalysisData(tid, uid, gid){
         document.body.appendChild(link); // Required for FF
         link.click();
     }
-    uxhr.send();
+    xhr.send();
 }
 
 function whoDidntFinish(tid, uid){
-    let link;
-    if(window.location.host == "1know.net") link = `http://1know.net/private/group/task/${tid}/analytics/unit/${uid}`;
-    else link = `http://www.1know.net/private/group/task/${tid}/analytics/unit/${uid}`;
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', link);
+    if(window.location.host == "1know.net") xhr.open('GET', `http://1know.net/private/group/task/${tid}/analytics/unit/${uid}`);
+    else xhr.open('GET', `http://www.1know.net/private/group/task/${tid}/analytics/unit/${uid}`);
+ 
     xhr.onload = function(){
         let data = JSON.parse(xhr.responseText);
         let reading = "", unread = "";
@@ -129,68 +195,7 @@ function getTasks() {
 }
 
 function init(){
-    const PLUGIN_STYLE = `
-    #KD_DIV{
-        border: 1px solid grey;
-        background-color: white;
-        font-family: 微軟正黑體;
-        box-shadow: 1px 2px 2px 2px grey;
-    }
-
-    .KD_HR{
-        border-top: 1px solid black;
-    }
-
-    .KD_BTN{
-        font-family: 微軟正黑體;
-        background-color: #037bfc;
-        border: none;
-        margin-left: 10px;
-        font-weight: bold;
-        margin-right: 10px;
-        border-radius: 5px;
-        color: white;
-        padding: 3px 8px 3px 8px;
-    }
-
-    .KD_BTN_2{
-        font-family: 微軟正黑體;
-        background-color: orange;
-        border: none;
-        margin-left: 10px;
-        font-weight: bold;
-        margin-right: 10px;
-        border-radius: 5px;
-        color: white;
-        padding: 3px 8px 3px 8px;
-    }
-
-    .KD_BTN:hover{
-        background-color: #0057b5;
-    }
-
-    .KD_LI{              
-        font-weight: bolder;
-        margin-bottom: 10px;
-        padding-bottom: 2px;
-        display: block;
-        padding-left: 15px;
-        border-bottom: 1px gray dotted;
-    }
-
-    .KD_H4{
-        font-weight: bold;
-        padding-left: 5px;
-        margin-top: 20px;
-    }
-
-    #KD_LOAD{
-        padding: 15px 0px 15px 0px;
-        font-famliy: 微軟正黑體;
-    }`;
-
     if(checkIs1Know() === true){
-
         // add init hint
 
         let styles = document.createElement('style'), loadDiv = document.createElement('div');
@@ -208,13 +213,13 @@ function init(){
         // init list
 
         let listDiv = document.createElement('div'), listContent;
-        listDiv.id = 'KD_DIV';
+        listDiv.id = "KD_DIV";
         TASK_LIST.map((task, i) => {
             listContent += `<h4 class='KD_H4'>【${task.name}】</h4>`;
             task.units.map((unit, j) => {
-                if(unit.unit_type == 'video') {
+                if(unit.unit_type == "video") {
                     listContent += `<div class='KD_LI'>${unit.name} 
-                    <button type="button" title="下載為CSV檔" onclick="getAnalysisData('${task.uqid}','${unit.uqid}','${groupID}')" class="KD_BTN">CSV</button>
+                    <button type="button" title="下載為CSV檔" onclick="getAnalysisData('${task.uqid}','${unit.uqid}')" class="KD_BTN">CSV</button>
                     <button type="button" title="查看誰沒看完影片" onclick="whoDidntFinish('${task.uqid}','${unit.uqid}')" class="KD_BTN_2">誰沒看完？</button></div>`
                 }
             });
